ff85cd39d480a31c59f13fde2826548b92c0b608
Jacob Mealey <jacobmealey.maine@gmail.com>
2022-06-15 14:33:17 -0400

    made text box scrollable

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, TextView};
+use gtk::{Application, ApplicationWindow, TextView, ScrolledWindow};
 
 fn main() {
     println!("Hello, world!");
@@ -19,20 +19,32 @@ fn build_ui(app: &Application) {
         .object("window")
         .expect("Could not get object 'window' from builder");
 
+
     let text: TextView = builder
         .object("text")
         .expect("Could not get object 'button' from builder");
+
+
+    let scroll: ScrolledWindow = builder
+        .object("scroll")
+        .expect("Could not get object 'scroll' from builder");
+
+    window.set_default_width(650);
+    window.set_default_height(420);
+    window.set_title(Some("Notes"));
 
     text.set_editable(true);
     
     window.set_application(Some(app));
 
+    // this is going to do something with a time for autosaving
     text.buffer().connect_changed(move |buff| {
-        let begin_itter = buff.start_iter();
-        let end_itter = buff.end_iter();
-        println!("{}", buff.text(&begin_itter, &end_itter, true));
+        //let begin_itter = buff.start_iter(); // << why was that so hard ??
+        //let end_itter = buff.end_iter();
+        //println!("{}", buff.text(&begin_itter, &end_itter, true));
     });
 
-    window.set_child(Some(&text));
+    scroll.set_child(Some(&text));
+    window.set_child(Some(&scroll));
     window.present();
 }
