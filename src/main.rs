e9f2bb15ef13d97882b6c10f683351970efbf769
Jacob Mealey <jacobmealey.maine@gmail.com>
2022-06-21 13:28:44 -0400

    cleaned up code and added comments

--- src/main.rs
+++ src/main.rs
@@ -26,54 +26,53 @@ fn build_ui(app: &Application) {
     .build();
 
     let header = HeaderBar::new();
-    window.set_titlebar(Some(&header));
     let grid: Grid = Grid::new();
-
     let note_count = Rc::new(RefCell::new(1));
-
     let stack_rc = Rc::new(Stack::new());
+    let sidebar: StackSidebar = StackSidebar::new();
+    let new_note_button = Button::new();
+
+    // set up stack and stacksidebar for organizing the screen
     stack_rc.set_hexpand(true);
     stack_rc.set_vexpand(true);
 
     // this is a very cursed line tbh. we are dereferencing the rc 
     // and the borrowing the stack
-    grid.attach(&*stack_rc, 1, 0, 1, 1);
-
-    let sidebar: StackSidebar = StackSidebar::new();
-    sidebar.set_stack(&*stack_rc);
-
+    grid.attach(&(*stack_rc), 1, 0, 1, 1);
     grid.attach(&sidebar, 0, 0, 1, 1);
+    sidebar.set_stack(&(*stack_rc));
 
-    let button = Button::new();
-    button.set_label("New");
+    // create a new note when user clicks the new_note_button
+    new_note_button.set_label("New");
+    new_note_button.connect_clicked(move |_| {
 
-    button.connect_clicked(move |_| {
+        // get references to existing state
         let mut update_count = note_count.borrow_mut();
         let rc = stack_rc.clone();
-        println!("Creating new note");
+
+        println!("Creating new note {}", update_count);
         let title = format!("New Note {}", update_count);
-        
+        let name = format!("new_note{}", update_count);
+        *update_count += 1;
+
+        // create a new noteview instance and bing 
         let scroll: ScrolledWindow = ScrolledWindow::new();
         let noteview: NoteViewObject = NoteViewObject::new();
         noteview.setup();
-        scroll.set_child(Some(&noteview));
 
-        let name = format!("new_note{}", update_count);
-        *update_count += 1;
+        scroll.set_child(Some(&noteview));
         rc.add_titled(&scroll, Some(&name), &title);
     });
 
-    header.pack_start(&button);
+    // add button to header
+    header.pack_start(&new_note_button);
 
+    // Set parameters for window settings
+    window.set_titlebar(Some(&header));
     window.set_default_width(650);
     window.set_default_height(420);
     window.set_title(Some("Notes"));
-    
     window.set_application(Some(app));
-
-    // scroll.set_child(Some(&text));
-    // stackpage.set_child(Some(&scroll))
-    // stack.set_child(Some(&stackpage));
     window.set_child(Some(&grid));
     window.present();
 }
