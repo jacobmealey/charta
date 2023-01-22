780292016c2170cfd4d9e1d9f8cd9495e8592369
Jacob Mealey <jacobmealey.maine@gmail.com>
2023-01-22 17:00:14 -0500

    added optional prefix to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
-INSTALL_DIR=/usr/bin/
-ASSETS_DIR=/usr/share/charta
+
+
+INSTALL_DIR=$1/usr/bin/
+ASSETS_DIR=$1/usr/share/charta
+echo "Installing to " $1
 
 if [[ !(-e assets/xyz.jacobmealey.Notes.desktop) ]]
 then
@@ -18,10 +21,10 @@ fi
 mkdir -p $INSTALL_DIR
 mkdir -p $ASSETS_DIR
 mkdir -p $ASSETS_DIR/json/
-cp assets/xyz.jacobmealey.Notes.desktop /usr/share/applications/xyz.jacobmealey.Notes.desktop
+cp assets/xyz.jacobmealey.Notes.desktop $0/usr/share/applications/xyz.jacobmealey.Notes.desktop
 cp assets/starter.txt $ASSETS_DIR/json/starter.txt
 cp assets/style.css $ASSETS_DIR/style.css
-cp assets/bitmap.png /usr/share/pixmaps/charta.png
+cp assets/bitmap.png $0/usr/share/pixmaps/charta.png
 # cp for eventual icon
 cp target/debug/notes $INSTALL_DIR/notes
 
